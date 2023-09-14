408e05d79f6d7be7dbe554092a76f24709da885a
intmain84 <aheleha@gmail.com>
2023-09-14 18:20:30 +0600

    fghfh

--- src/main.js
+++ src/main.js
@@ -6,12 +6,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import { OhVueIcon, addIcons } from 'oh-vue-icons'
-import { FaFlag, RiZhihuFill } from 'oh-vue-icons/icons'
-addIcons(FaFlag, RiZhihuFill)
-
 const app = createApp(App)
-app.component('v-icon', OhVueIcon)
 
 app.use(createPinia())
 app.use(router)
