f0296c140ce5be3b6e752aa6331cd4d0f5dc8644
intmain84 <aheleha@gmail.com>
2023-11-16 21:04:25 +0600

    sdfsdf

--- src/components/ChatPreview.vue
+++ src/components/ChatPreview.vue
@@ -30,7 +30,7 @@ const { chat } = toRefs(props)
           {{ chat.lastMessage.fromUser === store.currentUser ? 'You' : chat.nickname }}:
           {{ chat.lastMessage.text }}
         </div> -->
-        <div class="message-preview">{{ chat.status === true ? 'Online 🟢' : 'Offline 🔴' }}</div>
+        <div class="message-preview">{{ chat.status === true ? 'Xaxaxa 🟢' : 'Offline 🔴' }}</div>
       </div>
     </a>
   </li>
